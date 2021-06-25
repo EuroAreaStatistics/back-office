26ad52077a6fc725c491e977940cad1248ab656f
Alistair Cloete <cloete@adjoli.de>
2021-06-25 17:45:03 +0200

    fix previewURL

--- mainConfig.php
+++ mainConfig.php
@@ -2,7 +2,7 @@
 // absolute URL of server
 $baseURL = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS']!='off')?'https':'http') . "://$_SERVER[HTTP_HOST]";
 // URL for previews
-$previewURL = 'http:/127.0.0.1/';
+$previewURL = 'http://127.0.0.1';
 // theme
 $themeURL = 'default';
 
